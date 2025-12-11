9a52a37a82625f7519813f93e0f83794add85f90
Kyle M. Tarplee <kyle.tarplee@udri.udayton.edu>
2025-12-11 16:28:49 -0500

    fix(deps): update dependencies
    Includes updating x/crypto to mitigate GHSA-f6x5-jh6r-wrfv and GHSA-j5w8-q4qc-rx2x (not that NACK is actually vulnerable).
    
    Signed-off-by: Kyle M. Tarplee <kmtarplee@ieee.org>

--- pkg/jetstream/generated/applyconfiguration/jetstream/v1beta2/account.go
+++ pkg/jetstream/generated/applyconfiguration/jetstream/v1beta2/account.go
@@ -40,6 +40,7 @@ func Account(name, namespace string) *AccountApplyConfiguration {
 	b.WithAPIVersion("jetstream.nats.io/v1beta2")
 	return b
 }
+func (b AccountApplyConfiguration) IsApplyConfiguration() {}
 
 // WithKind sets the Kind field in the declarative configuration to the given value
 // and returns the receiver, so that objects can be built by chaining "With" function invocations.
@@ -215,8 +216,24 @@ func (b *AccountApplyConfiguration) WithStatus(value *StatusApplyConfiguration) 
 	return b
 }
 
+// GetKind retrieves the value of the Kind field in the declarative configuration.
+func (b *AccountApplyConfiguration) GetKind() *string {
+	return b.TypeMetaApplyConfiguration.Kind
+}
+
+// GetAPIVersion retrieves the value of the APIVersion field in the declarative configuration.
+func (b *AccountApplyConfiguration) GetAPIVersion() *string {
+	return b.TypeMetaApplyConfiguration.APIVersion
+}
+
 // GetName retrieves the value of the Name field in the declarative configuration.
 func (b *AccountApplyConfiguration) GetName() *string {
 	b.ensureObjectMetaApplyConfigurationExists()
 	return b.ObjectMetaApplyConfiguration.Name
+}
+
+// GetNamespace retrieves the value of the Namespace field in the declarative configuration.
+func (b *AccountApplyConfiguration) GetNamespace() *string {
+	b.ensureObjectMetaApplyConfigurationExists()
+	return b.ObjectMetaApplyConfiguration.Namespace
 }
